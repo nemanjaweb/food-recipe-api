46ac5a5e7119ef6cca93d4ad253b4f4907b6ff02
abc <abc@example.com>
2022-04-04 12:53:01 +0200

    get recipe

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
 //event listeners
 searchBtn.addEventListener("click", getMealList);
-mealList.addEventListener("click", getRecipe);
+mealList.addEventListener("click", getMealRecipe);
+recipeCloseBtn.addEventListener("click", () => {
+  mealDetailsContent.parentElement.classList.remove("showRecipe");
+});
 
 function getMealList() {
   let searchInput = document.getElementById("search-input").value.trim(); //trim() - removes whitespace.
@@ -40,8 +43,8 @@ function getMealList() {
     });
 }
 
-//get recipe
-function getRecipe(e) {
+// get recipe of the meal
+function getMealRecipe(e) {
   e.preventDefault();
   if (e.target.classList.contains("recipe-btn")) {
     let mealItem = e.target.parentElement.parentElement;
@@ -49,6 +52,30 @@ function getRecipe(e) {
       `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
     )
       .then((response) => response.json())
-      .then((data) => mealRecipe(data.meals));
+      .then((data) => mealRecipeModal(data.meals));
   }
+}
+
+// create a modal
+function mealRecipeModal(meal) {
+  console.log(meal);
+  meal = meal[0];
+  let html = `
+      
+      <h2 class = "recipe-title">${meal.strMeal}</h2>
+      <p class = "recipe-category">${meal.strCategory}</p>
+      <div class = "recipe-instruct">
+          <h3>Instructions:</h3>
+          <p>${meal.strInstructions}</p>
+      </div>
+      <div class = "recipe-meal-img">
+          <img src = "${meal.strMealThumb}" alt = "">
+      </div>
+      <div class = "recipe-link">
+          <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+      </div>
+      
+  `;
+  mealDetailsContent.innerHTML = html;
+  mealDetailsContent.parentElement.classList.add("showRecipe");
 }
